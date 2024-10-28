663e7d3d409ef8290412276d3bf57db9f42e7f6b
tyounami <vkbo@qq.com>
2024-10-28 23:48:20 +0800

    feat: Remove side-panel package

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -5,60 +5,50 @@ const packageJson = JSON.parse(fs.readFileSync('../package.json', 'utf8'));
 
 const isFirefox = process.env.__FIREFOX__ === 'true';
 
-const sidePanelConfig = {
-  side_panel: {
-    default_path: 'side-panel/index.html',
-  },
-  permissions: ['sidePanel'],
-};
-
 /**
  * After changing, please reload the extension at `chrome://extensions`
  * @type {chrome.runtime.ManifestV3}
  */
-const manifest = deepmerge(
-  {
-    manifest_version: 3,
-    name: 'Igloo Helper for staff use',
-    version: packageJson.version,
-    description: 'Chrome extension boilerplate developed with Vite, React and Typescript',
-    host_permissions: ['<all_urls>'],
-    permissions: ['storage', 'scripting', 'tabs', 'notifications'],
-    options_page: 'options/index.html',
-    background: {
-      service_worker: 'background.iife.js',
-      type: 'module',
-    },
-    action: {
-      default_popup: 'popup/index.html',
-      default_icon: 'icon-34.png',
-    },
-    icons: {
-      128: 'icon-128.png',
-    },
-    content_scripts: [
-      {
-        matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-        js: ['content/index.iife.js'],
-      },
-      {
-        matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-        js: ['content-ui/index.iife.js'],
-      },
-      {
-        matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-        css: ['content.css'], // public folder
-      },
-    ],
-    devtools_page: 'devtools/index.html',
-    web_accessible_resources: [
-      {
-        resources: ['*.js', '*.css', '*.svg', 'icon-128.png', 'icon-34.png'],
-        matches: ['*://*/*'],
-      },
-    ],
+const manifest = deepmerge({
+  manifest_version: 3,
+  name: 'Igloo Helper for staff use',
+  version: packageJson.version,
+  description: 'Chrome extension boilerplate developed with Vite, React and Typescript',
+  host_permissions: ['<all_urls>'],
+  permissions: ['storage', 'scripting', 'tabs', 'notifications'],
+  options_page: 'options/index.html',
+  background: {
+    service_worker: 'background.iife.js',
+    type: 'module',
   },
-  !isFirefox && sidePanelConfig,
-);
+  action: {
+    default_popup: 'popup/index.html',
+    default_icon: 'icon-34.png',
+  },
+  icons: {
+    128: 'icon-128.png',
+  },
+  content_scripts: [
+    {
+      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+      js: ['content/index.iife.js'],
+    },
+    {
+      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+      js: ['content-ui/index.iife.js'],
+    },
+    {
+      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+      css: ['content.css'], // public folder
+    },
+  ],
+  devtools_page: 'devtools/index.html',
+  web_accessible_resources: [
+    {
+      resources: ['*.js', '*.css', '*.svg', 'icon-128.png', 'icon-34.png'],
+      matches: ['*://*/*'],
+    },
+  ],
+});
 
 export default manifest;
