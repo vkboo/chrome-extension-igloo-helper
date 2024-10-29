ec97f44661568278e10474ba65b0681e4a9e3d7a
bo.zhao <bo.zhao@iglooinsure.com>
2024-10-29 09:59:49 +0800

    feat: Remove global content.css

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -30,10 +30,6 @@ const manifest = {
       matches: ['http://*/*', 'https://*/*', '<all_urls>'],
       js: ['content-ui/index.iife.js'],
     },
-    {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      css: ['content.css'], // public folder
-    },
   ],
   web_accessible_resources: [
     {
