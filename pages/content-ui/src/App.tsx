dfc8e82b4712faff66bc93e4a09d409290233116
bo.zhao <bo.zhao@iglooinsure.com>
2024-10-29 15:02:15 +0800

    chore: Remove useless logs

--- pages/content-ui/src/App.tsx
+++ pages/content-ui/src/App.tsx
@@ -1,14 +1,9 @@
-import { useEffect } from 'react';
 import { Button } from '@extension/ui';
 import { useStorage } from '@extension/shared';
 import { exampleThemeStorage } from '@extension/storage';
 
 export default function App() {
   const theme = useStorage(exampleThemeStorage);
-
-  useEffect(() => {
-    console.log('content ui loaded');
-  }, []);
 
   return (
     <div className="flex items-center justify-between gap-2 rounded bg-blue-100 px-2 py-1">
